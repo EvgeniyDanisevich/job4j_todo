722d59973bb755c611b381c77c0bdf52b498e32c
EvgeniyDanisevich <a937va@gmail.com>
2021-06-14 00:31:56 +0300

    hibernate.cfg.xml updated, users scheme added, item scheme updated

--- src/main/resources/init_item.sql
+++ src/main/resources/init_item.sql
@@ -1,5 +1,6 @@
 create table if not exists item (
     id serial primary key,
+    user_id int not null references users(id),
     description text,
     created timestamp,
     done boolean default false NOT NULL
